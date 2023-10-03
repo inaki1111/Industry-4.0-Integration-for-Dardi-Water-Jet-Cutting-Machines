6cbf933fa6e499b585c347ff2cece99870410bbc
Inaki <inakirm111@gmail.com>
2023-10-03 14:06:12 -0600

    local dashboard

---
+++ app.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Specify the full paths to your Python scripts
+script1="$HOME/send Data/startcutting.py"
+script2="$HOME/send Data/recieve_sensor_data.py"
+
+# MQTT subscription commands with the full path to mosquitto_sub
+command_run_machine_state="mosquitto_sub -t /machine_state"
+command_run_start_process="mosquitto_sub -t /start_process"
+command_run_Acceleration="mosquitto_sub -t /Acceleration"
+
+# Launch MQTT subscribers in separate terminal windows
+gnome-terminal -- bash -c "$command_run_machine_state; read -p 'Press Enter to exit'; exec bash"
+gnome-terminal -- bash -c "$command_run_start_process; read -p 'Press Enter to exit'; exec bash"
+gnome-terminal -- bash -c "$command_run_Acceleration; read -p 'Press Enter to exit'; exec bash"
+
+# Launch Python scripts in separate terminal windows
+gnome-terminal -- bash -c "python3 '$script1'; read -p 'Press Enter to exit'; exec bash"
+gnome-terminal -- bash -c "python3 '$script2'; read -p 'Press Enter to exit'; exec bash"
--- send Data/recieve_sensor_data.py
+++ send Data/recieve_sensor_data.py
@@ -1,51 +1,71 @@
-import datetime
 import csv
-import time
 import paho.mqtt.client as mqtt
 
-filename = "sensor_data.csv"
-fields = ["Time", "Encoder_X", "Encoder_Y", "Acceleration", "Current_1", "Current_2", "Current_3"]
+# MQTT topics to subscribe to
+topics = ["Time", "Encoder_X", "Encoder_Y", "Acceleration", "Current_1", "Current_2", "Current_3", "Tipo_Corte"]
 
-data_state = {field: None for field in fields[1:]}  # Do not include 'Time' as it will be populated separately
+# CSV file settings
+filename = "sensor_data.csv"
 
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
         print("Connected to broker")
-        global data_state
-        for field in fields[1:]:  # Subscribe to fields excluding 'Time'
-            client.subscribe(field)
+        for topic in topics:
+            client.subscribe(topic)
     else:
         print("Connection failed")
 
 def on_message(client, userdata, message):
-    print(f"Received message '{message.payload}' on topic '{message.topic}'")
-    global data_state
-    data_state[message.topic] = message.payload.decode('utf-8')
+    topic = message.topic
+    payload = message.payload.decode('utf-8')
     
-    if all(value is not None for value in data_state.values()):
-        data_state["Time"] = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        with open(filename, 'a', newline='') as csvfile:
-            csvwriter = csv.DictWriter(csvfile, fieldnames=fields)
-            csvwriter.writerow(data_state)
-        # Resetting data_state
-        data_state = {field: None for field in fields[1:]}
+    with open(filename, 'a', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow([topic, payload])
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
-client.connect("localhost", 1883, 60)  
+client.connect("localhost", 1883, 60)
 
 with open(filename, 'w', newline='') as csvfile:
-    csvwriter = csv.DictWriter(csvfile, fieldnames=fields)
-    csvwriter.writeheader()
+    csvwriter = csv.writer(csvfile)
 
-client.loop_start()
+client.loop_forever()
+import csv
+import paho.mqtt.client as mqtt
 
-try:
-    while True:
-        time.sleep(1)
-except KeyboardInterrupt:
-    print("Exiting...")
+# MQTT topics to subscribe to
+topics = ["Time", "Encoder_X", "Encoder_Y", "Acceleration", "Current_1", "Current_2", "Current_3", "Tipo_Corte"]
 
-client.loop_stop()
-client.disconnect()
+# CSV file settings
+filename = "sensor_data.csv"
+
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("Connected to broker")
+        for topic in topics:
+            client.subscribe(topic)
+            print("Subscribed to topic:", topic)
+    else:
+        print("Connection failed")
+
+def on_message(client, userdata, message):
+    topic = message.topic
+    payload = message.payload.decode('utf-8')
+    print(f"Received message on topic '{topic}': {payload}")
+    
+    with open(filename, 'a', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow([topic, payload])
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect("localhost", 1883, 60)
+
+with open(filename, 'w', newline='') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerow(["Topic", "Data"])  # Write header row
+
+client.loop_forever()
